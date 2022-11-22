882117f1d32c62dd29c236cfee38b473b8522f1e
Jan-jan <janjantandayag123@gmail.com>
2022-11-22 23:34:46 +0800

    Upload initial files - event emit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
+
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+  </div>
+  <p></p>
+  <button @click="toggleModal">show modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Modal},
+  data() {
+    return {
+      title: 'My First Vue App :)',
+      header: 'Sign Up for Jan-jan Giveaway!',
+      text: 'Enter and earn $50/day',
+      showModal: false
+    }
+  },
+
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
+
+    toggleModal(){
+      this.showModal = !this.showModal;
+    }
   }
 }
 </script>
@@ -22,5 +47,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
